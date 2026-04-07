bf6325e86f389b7a47ca93437e7c5e4bff9bc32f
millaamaiur <amaiur.milla@opendeusto.es>
2026-04-07 13:00:56 +0200

    Prueba main login

---
+++ admin/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+
+    /*He hecho esto como prueba pero mas adelante habrá que hacer dependiendo de
+    si la persona que hace login es admin o cliente mostrarle otro menu distinto*/
+    char usuario[50];
+    char password[50];
+
+    printf("Usuario: ");
+    scanf("%s", usuario);
+
+    printf("Password: ");
+    scanf("%s", password);
+
+    printf("\nHas introducido:\n");
+    printf("Usuario: %s\n", usuario);
+    printf("Password: %s\n", password);
+
+    return 0;
+}
